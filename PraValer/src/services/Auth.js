cf93670f0c9645d960ba7064b1026a8dfb6f4b37
MasterDihgo <132685239+MasterDihgo@users.noreply.github.com>
2023-06-21 00:28:57 -0300

    pra valer firebase e services

---
+++ PraValer/src/services/Auth.js
@@ -0,0 +1,61 @@
+import {Alert} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+
+export const setUserAuth = async () => {
+    await AsyncStorage.setItem('userAuth', uid);
+};
+
+export const getUserAuth = async () => {
+    const userAuth = await AsyncStorage.getItem('userAuth');
+    return userAuth;
+};
+
+export const cleanUserAuth = async () => {
+    await AsyncStorage.removeItem('userAuth');
+};
+
+export const signUp = async (data) => {
+    const {email, password, name} = data;
+    try {
+        const userInfos = await auth().createUserWithEmailAndPassword(
+            email,
+            password,
+        );
+
+        setUserAuth(userInfos.user.uid);
+
+        await firestore()
+        .collection('users')
+        .doc(userInfos.user.uid)
+        .set({
+            name: name,
+            email: email,
+            password: password,
+        })
+        return {registrerSuccess: true}
+    } catch (e) {
+        Alert.alert('Erro ao cadastrar usuário', e.message);
+        return {registrerSuccess: false}
+    }
+}
+
+export const signIn = async (data) => {
+    const {email, password} = data;
+
+    try {
+        const userInfos = await auth().signInWithEmailAndPassword(
+            email,
+            password,
+        );
+
+        setUserAuth(userInfos.user.uid);
+
+        return {loginSuccess: true};
+    } catch (e) {
+        Alert.alert('Erro ao tentar entrar');
+        console.log('signIn :: e: ', JSON.stringify(e.message));
+        return {loginSuccess: false}
+    }
+}
