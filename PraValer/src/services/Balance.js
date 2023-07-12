3b4286c32f1e429d88766f57802290486ba07eb8
MasterDihgo <masterdihgo@gmail.com>
2023-07-11 23:35:27 -0300

    pc travando

---
+++ PraValer/src/services/Balance.js
@@ -0,0 +1,24 @@
+import _ from 'lodash';
+import moment from '../vendors/moment';
+
+import firestore from "@react-native-firebase/firestore";
+
+import {getUserAuth} from './Auth';
+
+export const getBalance = async () => {
+    const userAuth = await getUserAuth();
+
+    let querySnapshot;
+
+    querySnapshot = await firestore()
+    .collection('entries')
+    .where('userId', '==', userAuth)
+    .orderBy('preco')
+    .get()
+
+    return _(querySnapshot.docs).reduce((total, doc) => {
+        return total + doc.data().preco;
+        }, 0);
+        
+
+}
